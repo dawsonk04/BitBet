6171e5f712d6ddd749f11947c63e02103c4c3548
Dawson Knudtson <300078204@fvtc.edu>
2024-09-18 16:56:01 -0500

    added some PL classes

--- JD.BitBet.PL/tblHand.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace JD.BitBet.PL
-{
-    public partial class tblHand
-    {
-        public Guid HandID { get; set; }
-        public Double BetAmount { get; set; }
-        public Double Result { get; set; }
-    }
-}
