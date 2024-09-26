b5306667a6eaff801d2faeb964f7e5ef5ff6f7b3
Dawson Knudtson <300078204@fvtc.edu>
2024-09-26 17:08:35 -0500

    couple test changes

---
+++ JD.BitBet.Test/utBase.cs
@@ -0,0 +1,69 @@
+﻿using JD.BitBet.PL.Data;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Storage;
+using Microsoft.Extensions.Configuration;
+
+namespace JD.BitBet.PL.Test
+{
+    [TestClass]
+    public class utBase<T> where T : class
+    {
+        protected BitBetEntities dc;  // declare the DataContext
+        protected IDbContextTransaction transaction;
+        private IConfigurationRoot _configuration;
+        private DbContextOptions<BitBetEntities> options;
+
+        public utBase()
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json");
+
+            _configuration = builder.Build();
+
+            options = new DbContextOptionsBuilder<BitBetEntities>()
+                .UseSqlServer(_configuration.GetConnectionString("BitBetConnection"))
+                .UseLazyLoadingProxies()
+                .Options;
+
+            dc = new BitBetEntities(options);
+        }
+
+        [TestInitialize]
+        public void TestInitialize()
+        {
+            transaction = dc.Database.BeginTransaction();
+        }
+
+        [TestCleanup]
+        public void TestCleanup()
+        {
+            transaction.Rollback();
+            transaction.Dispose();
+            dc = null;
+        }
+
+        public List<T> LoadTest()
+        {
+            return dc.Set<T>().ToList();
+        }
+
+        public int InsertTest(T row)
+        {
+            dc.Set<T>().Add(row);
+            return dc.SaveChanges();
+        }
+
+        public int UpdateTest(T row)
+        {
+            dc.Entry(row).State = EntityState.Modified;
+            return dc.SaveChanges();
+        }
+
+        public int DeleteTest(T row)
+        {
+            dc.Set<T>().Remove(row);
+            return dc.SaveChanges();
+        }
+    }
+}
