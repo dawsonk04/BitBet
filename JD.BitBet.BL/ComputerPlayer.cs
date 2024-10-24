78a50edc73be65c1c8a68828c211fdc7fbe84f0c
Dawson Knudtson <300078204@fvtc.edu>
2024-10-24 13:38:33 -0500

    inital

---
+++ JD.BitBet.BL/ComputerPlayer.cs
@@ -0,0 +1,31 @@
+﻿namespace JD.BitBet.BL
+{
+    public class ComputerPlayer
+    {
+        private readonly DealerManager _dealerManager;
+
+        public ComputerPlayer(DealerManager dealerManager)
+        {
+            _dealerManager = dealerManager;
+        }
+
+        public GameResult PlayHand()
+        {
+            var playerHand = _dealerManager.GetPlayerHand();
+            var handValue = _dealerManager.CalculateHandValue(playerHand.ToList());
+
+            // Basic strategy implementation
+            while (handValue < 17)
+            {
+                var result = _dealerManager.PlayerHit();
+                if (result != GameResult.InProgress)
+                    return result;
+
+                playerHand = _dealerManager.GetPlayerHand();
+                handValue = _dealerManager.CalculateHandValue(playerHand.ToList());
+            }
+
+            return _dealerManager.PlayerStand();
+        }
+    }
+}
