3226b316dc49629a44111b094473df8552eba142
John Strange <300089261@fvtc.edu>
2024-10-24 14:10:20 -0500

    DId some stuff

---
+++ JD.BitBet.BL.Test/utDealerManager.cs
@@ -0,0 +1,100 @@
+﻿using static JD.BitBet.BL.Models.Cards;
+
+namespace JD.BitBet.BL.Test
+{
+    [TestClass]
+    public class utDealerManager
+    {
+        private Deck _deck;
+        private DealerManager _dealerManager;
+
+        [TestInitialize] // Use [SetUp] for NUnit
+        public void Setup()
+        {
+            _deck = new Deck();
+            _dealerManager = new DealerManager(_deck);
+        }
+
+        [TestMethod] // Use [Test] for NUnit
+        public void StartNewGame_DealsFourCards()
+        {
+            // Arrange
+            double betAmount = 100;
+
+            // Act
+            _dealerManager.StartNewGame(betAmount);
+
+            // Assert
+            Assert.AreEqual(2, _dealerManager.GetPlayerHand().Count);
+            Assert.AreEqual(2, _dealerManager.GetDealerHand().Count);
+        }
+
+        [TestMethod]
+        public void PlayerHit_WhenOver21_ReturnsBust()
+        {
+            // Arrange
+            _dealerManager.StartNewGame(100);
+
+            // Act
+            _dealerManager.PlayerHit();
+            _dealerManager.PlayerHit();
+            _dealerManager.PlayerHit();
+            _dealerManager.PlayerHit();
+            _dealerManager.PlayerHit();
+            var result = _dealerManager.PlayerHit();
+            // Assert
+            Assert.AreEqual(GameResult.PlayerBust, result);
+        }
+
+        [TestMethod]
+        public void CanDoubleDown_WithInitialHandValueOf11_ReturnsTrue()
+        {
+            // Arrange
+            _dealerManager.StartNewGame(100);
+            var playerHand = _dealerManager.GetPlayerHand().ToList();
+            playerHand.Clear();
+            playerHand.Add(new Card(Rank.Five, Suit.Hearts));
+            playerHand.Add(new Card(Rank.Five, Suit.Spades));
+
+            // Act
+            var canDouble = _dealerManager.CanDoubleDown();
+
+            // Assert
+            Assert.IsTrue(canDouble);
+        }
+
+        [TestMethod]
+        public void CalculateHandValue_WithAceAndKing_Returns21()
+        {
+            // Arrange
+            var hand = new List<Card>
+        {
+            new Card(Rank.Ace, Suit.Hearts),
+            new Card(Rank.King, Suit.Spades)
+        };
+
+            // Act
+            var result = _dealerManager.CalculateHandValue(hand);
+
+            // Assert
+            Assert.AreEqual(21, result);
+        }
+
+        [TestMethod]
+        public void CalculateHandValue_WithTwoAces_Returns12()
+        {
+            // Arrange
+            var hand = new List<Card>
+        {
+            new Card(Rank.Ace, Suit.Hearts),
+            new Card(Rank.Ace, Suit.Spades)
+        };
+
+            // Act
+            var result = _dealerManager.CalculateHandValue(hand);
+
+            // Assert
+            Assert.AreEqual(12, result);
+        }
+    }
+}
---
+++ JD.BitBet.BL/DealerManager.cs
@@ -0,0 +1,132 @@
+﻿using JD.BitBet.BL.Models;
+using static JD.BitBet.BL.Models.Cards;
+
+namespace JD.BitBet.BL
+{
+    public class DealerManager
+    {
+        private readonly Deck _deck;
+        private List<Card> _playerHand;
+        private List<Card> _dealerHand;
+        private double _betAmount;
+        private const int BlackjackValue = 21;
+
+        public DealerManager(Deck deck)
+        {
+            _deck = deck;
+            _playerHand = new List<Card>();
+            _dealerHand = new List<Card>();
+        }
+
+        public void StartNewGame(double betAmount)
+        {
+            _betAmount = betAmount;
+            _playerHand.Clear();
+            _dealerHand.Clear();
+            _deck.Shuffle();
+
+            // Initial deal
+            _playerHand.Add(_deck.Deal());
+            _dealerHand.Add(_deck.Deal());
+            _playerHand.Add(_deck.Deal());
+            _dealerHand.Add(_deck.Deal());
+        }
+
+        public bool CanDoubleDown()
+        {
+            return _playerHand.Count == 2;
+        }
+
+        public GameResult DealerHit()
+        {
+            _playerHand.Add(_deck.Deal());
+            int handValue = CalculateHandValue(_playerHand);
+
+            if (handValue > BlackjackValue)
+                return GameResult.PlayerBust;
+
+            if (handValue == BlackjackValue)
+                return GameResult.PlayerBlackjack;
+
+            return GameResult.InProgress;
+        }
+
+        public GameResult PlayerDoubleDown()
+        {
+            if (!CanDoubleDown())
+                throw new InvalidOperationException("Double down is not allowed in current situation");
+
+            _betAmount *= 2;
+            _playerHand.Add(_deck.Deal());
+
+            int handValue = CalculateHandValue(_playerHand);
+            if (handValue > BlackjackValue)
+                return GameResult.PlayerBust;
+
+            return CompleteDealerTurn();
+        }
+
+        public GameResult PlayerStand()
+        {
+            return CompleteDealerTurn();
+        }
+
+        private GameResult CompleteDealerTurn()
+        {
+            while (CalculateHandValue(_dealerHand) < 17)
+            {
+                _dealerHand.Add(_deck.Deal());
+            }
+
+            int dealerValue = CalculateHandValue(_dealerHand);
+            int playerValue = CalculateHandValue(_playerHand);
+
+            if (dealerValue > BlackjackValue)
+                return GameResult.DealerBust;
+
+            if (dealerValue > playerValue)
+                return GameResult.DealerWins;
+
+            if (playerValue > dealerValue)
+                return GameResult.PlayerWins;
+
+            return GameResult.Push;
+        }
+
+        public int CalculateHandValue(List<Card> hand)
+        {
+            int value = 0;
+            int aceCount = 0;
+
+            foreach (var card in hand)
+            {
+                if (card.Rank == Rank.Ace)
+                    aceCount++;
+                value += (int)card.Rank;
+            }
+
+            while (value > BlackjackValue && aceCount > 0)
+            {
+                value -= 10;
+                aceCount--;
+            }
+
+            return value;
+        }
+
+        public IReadOnlyList<Card> GetPlayerHand() => _playerHand.AsReadOnly();
+        public IReadOnlyList<Card> GetDealerHand() => _dealerHand.AsReadOnly();
+        public double GetBetAmount() => _betAmount;
+    }
+
+    public enum GameResult
+    {
+        InProgress,
+        PlayerBlackjack,
+        PlayerBust,
+        DealerBust,
+        PlayerWins,
+        DealerWins,
+        Push
+    }
+}
