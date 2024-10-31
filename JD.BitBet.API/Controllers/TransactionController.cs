6b09a11bc5076d152e7ee886e44e9aa610d0f12e
John Strange <300089261@fvtc.edu>
2024-10-31 10:57:35 -0500

    Fixed TransactionController Error

--- JD.BitBet.API/Controllers/TransactionController.cs
+++ JD.BitBet.API/Controllers/TransactionController.cs
@@ -8,7 +8,7 @@ namespace JD.BitBet.API.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
-    public class TransactionController : GenericController<JD.BitBet.BL.Models.Transaction, TransactionManager, BitBetEntities>
+    public class TransactionController : GenericController<JD.BitBet.BL.Models.Transaction, JD.BitBet.BL.TransactionManager, BitBetEntities>
     {
         public TransactionController(ILogger<TransactionController> logger, DbContextOptions<BitBetEntities> options) : base(logger, options) { }
 
