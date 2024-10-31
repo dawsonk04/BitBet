12b1da610610601b7caa895e7ec4a2427866b73d
John Strange <300089261@fvtc.edu>
2024-10-31 11:18:40 -0500

    API Project is working

--- JD.BitBet.API/Program.cs
+++ JD.BitBet.API/Program.cs
@@ -1,25 +1,81 @@
-var builder = WebApplication.CreateBuilder(args);
+using JD.BitBet.PL.Data;
+using Microsoft.EntityFrameworkCore;
+using Serilog;
+using Serilog.Ui.MsSqlServerProvider;
+using Serilog.Ui.Web;
 
-// Add services to the container.
-
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+public class Program
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    private static void Main(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
+
+        // Add services to the container.
+        //Add SignalR config
+        builder.Services.AddSignalR().AddJsonProtocol(options =>
+        {
+            options.PayloadSerializerOptions.PropertyNamingPolicy = null;
+        });
+        builder.Services.AddControllers();
+        // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+        builder.Services.AddEndpointsApiExplorer();
+        builder.Services.AddSwaggerGen();
+
+        builder.Services.AddDbContextPool<BitBetEntities>(options =>
+        {
+            options.UseSqlServer(builder.Configuration.GetConnectionString("BitBetConnection"));
+        });
+
+        string connection = builder.Configuration.GetConnectionString("BitBetConnection");
+
+
+        builder.Services.AddSerilogUi(options =>
+        {
+            options.UseSqlServer(connection, "Logs");
+        });
+
+        var configSettings = new ConfigurationBuilder()
+            .AddJsonFile("appsettings.json")
+            .Build();
+
+
+        Log.Logger = new LoggerConfiguration()
+            .ReadFrom.Configuration(configSettings)
+            .CreateLogger();
+
+        builder.Services
+            .AddLogging(c => c.AddDebug())
+            .AddLogging(c => c.AddSerilog())
+            .AddLogging(c => c.AddEventLog())
+            .AddLogging(c => c.AddConsole());
+
+
+        var app = builder.Build();
+
+        app.UseSerilogUi(options =>
+        {
+            options.RoutePrefix = "logs";
+        });
+
+
+        // Configure the HTTP request pipeline.
+        if (app.Environment.IsDevelopment())
+        {
+            app.UseSwagger();
+            app.UseSwaggerUI();
+        }
+
+        app.UseHttpsRedirection();
+
+        app.UseRouting();
+        app.UseAuthorization();
+
+        // app.MapControllers();
+        //app.UseEndpoints(endpoints =>
+        //{
+        //    endpoints.MapControllers();
+        //    endpoints.MapHub<BingoHub>("/bingohub");
+        //});
+        app.Run();
+    }
 }
-
-app.UseHttpsRedirection();
-
-app.UseAuthorization();
-
-app.MapControllers();
-
-app.Run();
