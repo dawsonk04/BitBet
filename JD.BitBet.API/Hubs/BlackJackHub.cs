a292ae180f61def49885eb378bff08c2945f2a66
Dawson Knudtson <300078204@fvtc.edu>
2024-11-26 19:20:20 -0600

    signalR addition

--- JD.BitBet.API/Hubs/BlackJackHub.cs
+++ JD.BitBet.API/Hubs/BlackJackHub.cs
@@ -1,5 +1,5 @@
-﻿using Microsoft.AspNetCore.SignalR;
-using System.IO;
+﻿using JD.BitBet.BL.Models;
+using Microsoft.AspNetCore.SignalR;
 
 namespace JD.BitBet.API.Hubs
 {
@@ -9,5 +9,23 @@ namespace JD.BitBet.API.Hubs
         {
             await Clients.All.SendAsync("ReceiveMessage", User, message);
         }
+
+        public async Task JoinGame(string gameId)
+        {
+            await Groups.AddToGroupAsync(Context.ConnectionId, gameId);
+            await Clients.Group(gameId).SendAsync("PlayerJoined", Context.ConnectionId);
+        }
+
+        public async Task UpdateGameState(string gameId, GameState gameState)
+        {
+            await Clients.Group(gameId).SendAsync("GameStateUpdated", gameState);
+        }
+
+        public async Task LeaveGame(string gameId)
+        {
+            await Groups.RemoveFromGroupAsync(Context.ConnectionId, gameId);
+            await Clients.Group(gameId).SendAsync("PlayerLeft", Context.ConnectionId);
+        }
+
     }
 }
