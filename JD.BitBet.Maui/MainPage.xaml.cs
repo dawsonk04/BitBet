f98de345a269f14173cf9cb81840d09d4cd31896
Dawson Knudtson <300078204@fvtc.edu>
2024-12-09 23:02:51 -0600

    maui ui change

--- JD.BitBet.Maui/MainPage.xaml.cs
+++ JD.BitBet.Maui/MainPage.xaml.cs
@@ -2,24 +2,24 @@
 {
     public partial class MainPage : ContentPage
     {
-        int count = 0;
-
         public MainPage()
         {
             InitializeComponent();
         }
 
-        private void OnCounterClicked(object sender, EventArgs e)
+        private void OnLoginClicked(object sender, EventArgs e)
         {
-            count++;
 
-            if (count == 1)
-                CounterBtn.Text = $"Clicked {count} time";
-            else
-                CounterBtn.Text = $"Clicked {count} times";
+        }
 
-            SemanticScreenReader.Announce(CounterBtn.Text);
+        private void OnForgotPasswordTapped(object sender, TappedEventArgs e)
+        {
+
+        }
+
+        private void OnSignUpTapped(object sender, TappedEventArgs e)
+        {
+
         }
     }
-
 }
