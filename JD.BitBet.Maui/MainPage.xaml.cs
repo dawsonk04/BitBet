bf3145e13ffc6192be8362b29194e2aa60e9ed4c
Dawson Knudtson <300078204@fvtc.edu>
2024-12-10 20:36:41 -0600

    maui changes - login

--- JD.BitBet.Maui/MainPage.xaml.cs
+++ JD.BitBet.Maui/MainPage.xaml.cs
@@ -1,14 +1,56 @@
-﻿namespace JD.BitBet.Maui
+﻿using JD.BitBet.BL.Models;
+using JD.Utility;
+using Newtonsoft.Json;
+using System.Text;
+
+namespace JD.BitBet.Maui
 {
     public partial class MainPage : ContentPage
     {
+        private readonly ApiClient _client;
         public MainPage()
         {
             InitializeComponent();
+            var apiBaseUrl = "https://localhost:7061/swagger/index.html";
+            _client = new ApiClient(apiBaseUrl);
         }
 
-        private void OnLoginClicked(object sender, EventArgs e)
+        private async void OnLoginClicked(object sender, EventArgs e)
         {
+            var user = new User
+            {
+                Email = EmailEntry.Text,
+                Password = PasswordEntry.Text
+            };
+
+            string jsonContent = JsonConvert.SerializeObject(user);
+            var httpContent = new StringContent(jsonContent, Encoding.UTF8, "application/json");
+
+            var response = await _client.PostAsync("User/Authenticate", httpContent);
+            var responseContent = await response.Content.ReadAsStringAsync();
+
+            if (response.IsSuccessStatusCode)
+            {
+                var authenticatedUser = JsonConvert.DeserializeObject<User>(responseContent);
+                if (authenticatedUser != null)
+                {
+                    Preferences.Set("UserId", authenticatedUser.Id.ToString());
+
+                    await Navigation.PushAsync(new GameListPage());
+                    return;
+                }
+                else
+                {
+                    ErrorMessage.Text = "Error: Unable to parse user details.";
+                    ErrorMessage.IsVisible = true;
+                }
+            }
+            else
+            {
+                ErrorMessage.Text = $"Login failed. {response.StatusCode}: {responseContent}";
+                ErrorMessage.IsVisible = true;
+            }
+
 
         }
 
